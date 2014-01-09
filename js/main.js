1453cd5c59159c7f46c27a495949475a53c02b7a
Gonzalo Traverso <gonzalotraverso@gmail.com>
2014-01-09 01:00:37 -0300

    added second layer of parallax

--- js/main.js
+++ js/main.js
@@ -69,20 +69,18 @@
 	var ParallaxScrolling = function(resizer){
 		this.$win = resizer.$win;
 		this.$sections = $('section');
+		this.$secLvl = this.$sections.find(".scroll-second-lvl");
 		this.resizer = resizer;
 		this.init();
 	}
 
 	ParallaxScrolling.prototype = {
 		init: function(){
-			this.winH = this.$win.height();
-			this.$sections.css({top : this.winH});
-			this.$active = this.$sections.siblings('.section-1');
-			this.$active.css({top : 0});
-
-			this.acc = 5;
-
-			this.bindEvents();
+			if (this.resizer.$win.width() > 768) {
+				
+				this.setUp();
+				this.bindEvents();
+			};
 		},
 		bindEvents: function(){
 			var self = this;
@@ -90,32 +88,64 @@
 				self.moveLayers(event);
 			});
 		},
+		setUp: function(){
+			var self = this;
+			this.winH = this.$win.height();
+			this.$sections.css({top : this.winH});
+			this.$active = this.$sections.first();
+			this.$active.css({top : 0});
+
+			this.$secLvl.addClass('sec-lvl-style').each(function(){
+				var $sec = $(this).parents("section");
+				var top = $sec.offset().top;
+				console.log($sec.innerHeight());
+				if($sec.prev("section").length)
+					$(this).css({top : (self.winH * 0.18) });
+				else
+					$(this).css({top : 0 });
+			});
+
+
+			this.acc = 5;
+		},
 		moveLayers: function(event){
-			if (this.resizer.$win.width() > 768) {
-				event.preventDefault();
+			event.preventDefault();
 
-				var top = this.$active.offset().top;
-				var bottom = this.$active.height() + top;
-				this.actHeight = this.$active.height();
+			var top = this.$active.offset().top;
+			var bottom = this.$active.height() + top;
+			this.actHeight = this.$active.height();
 
-				var speed = this.acc * event.deltaFactor;
+			var $secLvlPrev = this.$active.prev("section").find(".scroll-second-lvl");
+			if($secLvlPrev.length)
+				var secTopPrev = parseInt($secLvlPrev.css("top"));
 
-				if(event.deltaY < 0){
-					if(bottom <= this.winH && this.$active.next("section").length){
-						this.$active = this.$active.next("section");
-					}else if(bottom > this.winH){
-						var topn = (bottom - speed < this.winH) ? this.winH - this.actHeight : top - speed;
-						this.$active.css({top : topn});
-					}
-				}else{
-					if(top >= this.winH && this.$active.prev("section").length){
-						this.$active = this.$active.prev("section");
-					}else if(top < this.winH && this.$active.prev("section").length){
-						var topn = (top + speed > this.winH) ? this.winH : top + speed;
-						this.$active.css({top : topn});
-					}
+
+			var $sec = this.$active.find(".scroll-second-lvl");
+			if($sec.length)
+				var sec = parseInt($sec.css("top"));
+			
+			var speed = this.acc * event.deltaFactor;
+
+			if(event.deltaY < 0){
+				if(bottom <= this.winH && this.$active.next("section").length){
+					this.$active = this.$active.next("section");
+				}else if(bottom > this.winH){
+					var topn = (bottom - speed < this.winH) ? this.winH - this.actHeight : top - speed;
+					this.$active.css({top : topn});
+					$sec.css({top : sec - event.deltaFactor});
+					$secLvlPrev.css({top : secTopPrev - event.deltaFactor});
 				}
-			};
+			}else{
+				if(top >= this.winH && this.$active.prev("section").length){
+					this.$active = this.$active.prev("section");
+				}else if(top < this.winH && this.$active.prev("section").length){
+					var topn = (top + speed > this.winH) ? this.winH : top + speed;
+					this.$active.css({top : topn});
+					$sec.css({top : sec + event.deltaFactor});
+
+					$secLvlPrev.css({top : secTopPrev + event.deltaFactor});
+				}
+			}
 		}
 	}
 
